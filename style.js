9b6947819bc4df8a89686a15c29844c1334dcbd4
Ishita Mukherjee <ishitam299@gmail.com>
2023-11-20 14:43:25 +0530

    Update style.js

--- style.js
+++ style.js
@@ -3,6 +3,7 @@ let buttons = document.querySelectorAll('button');
 
 let string = "";
  let arr = Array.from(buttons);
+ 
 arr.forEach(button=>{
     button.addEventListener('click', (event)=>{
         if(event.target.innerHTML == '='){
