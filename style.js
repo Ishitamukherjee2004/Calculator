5ffd4e9153adfa2156be29e6cb53b2e5a4a8df28
Ishita Mukherjee <ishitam299@gmail.com>
2024-05-07 21:14:25 +0530

    Some bugs are fixed

--- style.js
+++ style.js
@@ -18,20 +18,6 @@ arr.forEach(button=>{
             string = string.substring(0, string.length-1);
             input.value = string;
         }
-        else if(event.target.innerHTML=='%'){
-            let operators = ['+', '-', '*', '/'];
-            let lastOperatorIndex = -1;
-            for (let i = string.length - 1; i >= 0; i--) {
-                if (operators.includes(string[i])) {
-                    lastOperatorIndex = i;
-                    break;
-                }
-            }
-            let number = parseFloat(string.substring(lastOperatorIndex + 1));
-            let percentage = number * 0.01;
-            string = percentage.toString();
-            input.value = string; 
-        }
         else{
             string += event.target.innerHTML;
             input.value = string;
